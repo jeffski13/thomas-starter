0b3440db7487b53c85af319a52e212a056ae73e1
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-06-13 11:51:26 -0500

    added a pokedex to the homepage

--- app/homePage/index.tsx
+++ app/homePage/index.tsx
@@ -1,11 +1,11 @@
-import { Container, Row, Col, Image } from "react-bootstrap";
 import './styles.css';
 
 export default function HomePage() {
 
   return (
-    <div className="homePage" >
-      <div>Good afternoon Professor Thomas.</div>
+    <div className="homePage">
+      <div className="homePage-greeting">Good afternoon Professor Thomas.</div>
+      <img src="/images/pokedex-icon.png" alt="Pokedex" className="homePage-icon" />
     </div>
   );
 }
