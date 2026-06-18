fd8b2839e96680e8c04d0d833bafba2c6d40bfa2
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-06-18 14:59:28 -0500

    insane mario hat

--- app/homePage/index.tsx
+++ app/homePage/index.tsx
@@ -4,17 +4,42 @@ import './styles.css';
 
 export default function HomePage() {
   const [jefffyColor, setJeffyColor] = useState("green");
+  const [jefffySize, setJeffySize] = useState("0.8rem");
 
   function randomColor() {
     return `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, "0")}`;
   }
 
+  function randomSize() {
+    return `${(Math.random() * 10).toFixed(1)}rem`;
+  }
+
+  const [hatFlying, setHatFlying] = useState(false);
+
+  function handlePokedexClick() {
+    setJeffyColor(randomColor());
+    setJeffySize(randomSize());
+  }
+
+  function handleHatClick() {
+    if (hatFlying) return;
+    const audio = new Audio("/sadTrombone.mp3");
+    setHatFlying(true);
+    audio.play();
+    audio.addEventListener("ended", () => setHatFlying(false));
+  }
+
   return (
     <div className="homePage" >
       <div>Good afternoon Professor Thomas.</div>
-      <div className="uncleJeffy" style={{ color: jefffyColor }}>Actually, Im uncle Jeffy</div>
-      <Image src="/images/pokedex-icon.png" alt="Pokedex" 
-       onClick={() => setJeffyColor(randomColor())} style={{ cursor: "pointer" }} 
+      <div className="uncleJeffy" style={{ color: jefffyColor, fontSize: jefffySize }}>Actually, Im uncle Jeffy</div>
+      <Image src="/images/pokedex-icon.png" alt="Pokedex"
+       onClick={handlePokedexClick} style={{ cursor: "pointer", width: "150px" }}
+      />
+      <Image src="/images/marioHat.png" alt="Mario Hat"
+        className={hatFlying ? "marioHatFlying" : ""}
+        style={{ width: "150px", cursor: "pointer" }}
+        onClick={handleHatClick}
       />
     </div>
   );
