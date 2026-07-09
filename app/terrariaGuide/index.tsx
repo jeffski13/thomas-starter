e58278e8816646cc6690cf5b596a0ddbbeab4a4e
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-07-09 12:16:30 -0500

    requirements list

--- app/terrariaGuide/index.tsx
+++ app/terrariaGuide/index.tsx
@@ -1,3 +1,5 @@
+import { ListGroup } from "react-bootstrap";
+
 interface GuideSection {
   title: string;
   items: string[];
@@ -7,9 +9,17 @@ interface GuideSection {
 export default function terrariaGuide() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1.5rem", marginTop: "1.5rem", color: "#333", padding: "0 1rem" }}>
-      <h2 style={{ margin: 0 }}>Terraria Guide</h2>
-      <h5>Coming Soon</h5>
-      
+      <h2 style={{ margin: 0 }}>Pokemon Guide</h2>
+      <h3>how to get a master ball in blue version</h3>
+      <h5>Requirements</h5>
+      <ListGroup>
+        <ListGroup.Item>Saffron City Access</ListGroup.Item>
+        <ListGroup.Item>Level 30+ pokemon team</ListGroup.Item>
+        <ListGroup.Item>Flying Pokemon</ListGroup.Item>
+        <ListGroup.Item>Cleared Celadon Game Corner</ListGroup.Item>
+        <ListGroup.Item>10 Super Potions</ListGroup.Item>
+      </ListGroup>
+
     </div>
   );
 }
