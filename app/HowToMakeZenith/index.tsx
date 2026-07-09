a7d0a4f9d5cc27094b2cfe9b7d3e7f9b7ef778c1
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-07-09 12:52:21 -0500

    images

--- app/HowToMakeZenith/index.tsx
+++ app/HowToMakeZenith/index.tsx
@@ -7,16 +7,47 @@ export default function HowToMakeZenithPage() {
       <p style={{ color: "#072c71" }}>Coming soon.</p>
       <h2>Materials list</h2>
     <ListGroup>
-      <ListGroup.Item>true nights edge</ListGroup.Item>
-      <ListGroup.Item>copper shortsword</ListGroup.Item>
-      <ListGroup.Item>enchanted sword</ListGroup.Item>
-      <ListGroup.Item>starfury</ListGroup.Item>
-      <ListGroup.Item>starwrath</ListGroup.Item>
-      <ListGroup.Item>meowmere</ListGroup.Item>
-      <ListGroup.Item>seedler</ListGroup.Item>
-      <ListGroup.Item>beekeeper</ListGroup.Item>
-      <ListGroup.Item>horsemans blade</ListGroup.Item>
-      <ListGroup.Item>terra blade</ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/True_NightEdge.webp" alt="true nights edge" height={32} />
+        true nights edge
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/copper_shortsword.jpg" alt="copper shortsword" height={32} />
+        copper shortsword
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/EnchantedSword.webp" alt="enchanted sword" height={32} />
+        enchanted sword
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/star-fury.webp" alt="starfury" height={32} />
+        starfury
+      </ListGroup.Item>
+      
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/starwrath.png" alt="meowmere" height={32} />
+        starwrath
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/meowmere.png" alt="meowmere" height={32} />
+        meowmere
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/Seedler.webp" alt="seedler" height={32} />
+        seedler
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/Beekeeper.png" alt="beekeeper" height={32} />
+        beekeeper
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/horsemansblade.png" alt="horsemans blade" height={32} />
+        horsemans blade
+      </ListGroup.Item>
+      <ListGroup.Item className="d-flex align-items-center gap-2">
+        <img src="/images/zenith/TerraBlade.webp" alt="terra blade" height={32} />
+        terra blade
+      </ListGroup.Item>
     </ListGroup>
     </div>
   );
