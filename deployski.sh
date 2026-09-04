dc8f33a3edb610c7a2d99404c5d8dda0f76a3c60
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-09-04 09:31:01 -0500

    Update deployski.sh

--- deployski.sh
+++ deployski.sh
@@ -36,4 +36,4 @@ npmVersion=$(node -p "require('./package.json').version")
 git add ./.firebase/hosting.YnVpbGQvY2xpZW50.cache
 git commit -m "firebase cache for release. v$npmVersion"
 
-# git push --tags origin main
+git push --tags origin main
